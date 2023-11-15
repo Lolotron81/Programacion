e03736655f9c67c93b00ad4d196b98817693af11
Lolotron81 <a_manuel.leal.medina@iespablopicasso.es>
2023-11-15 23:36:12 +0100

    Añado Unidad 04

---
+++ Unidad 04. Desarrollo de Clases/Ejercicios/Actividad 401 - Clases y Atributos/u04r01ClasesyAtributos/src/prog/unidad04/atributos/Pizza.java
@@ -0,0 +1,10 @@
+package prog.unidad04.atributos;
+
+public class Pizza {
+	
+	public String tamañoPizza;
+	public String tipoPizza;
+	public char servidaONo;
+	public int pizzasCreadas;
+	public int pizzasServidas;
+}
